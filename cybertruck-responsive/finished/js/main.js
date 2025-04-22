ddd889da54f474e29ca254ecb95a0bbaec7bcb71
Erik <e.katerborg@hr.nl>
2025-04-22 14:37:37 +0200

    Delete cybertruck

--- cybertruck-responsive/finished/js/main.js
+++
@@ -1,8 +0,0 @@
-let contactLink = document.getElementById("btn-specs");
-
-contactLink.addEventListener("click", function(){
-    console.log("je klikt op de specs button");
-
-    let specsSection = document.getElementById("specs");
-    specsSection.scrollIntoView({ behavior: 'smooth' });
-});
